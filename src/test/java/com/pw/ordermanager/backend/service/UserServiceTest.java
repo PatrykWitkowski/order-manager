402320e9589c4b3c366160d48c5b0fe1f7653def
Patryk Witkowski <218167@student.pwr.edu.pl>
2018-09-26 18:57:48 +0200

    added unit tests for feature's branch products changes

--- src/test/java/com/pw/ordermanager/backend/service/UserServiceTest.java
+++ src/test/java/com/pw/ordermanager/backend/service/UserServiceTest.java
@@ -2,9 +2,13 @@ package com.pw.ordermanager.backend.service;
 
 import com.pw.ordermanager.backend.common.UserType;
 import com.pw.ordermanager.backend.dts.UserDts;
+import com.pw.ordermanager.backend.entity.Product;
 import com.pw.ordermanager.backend.entity.User;
 import com.pw.ordermanager.backend.jpa.UserRepository;
 import com.pw.ordermanager.backend.utils.security.SecurityUtils;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinSession;
+import org.assertj.core.util.Lists;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,8 +19,8 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -28,6 +32,7 @@ public class UserServiceTest {
     public static final String ADMIN_USERNAME = "admin";
     public static final String WRONG_PASSWORD = "wrong";
     public static final String TOKEN = "12ds-fsdf-4fds-ge9s";
+    public static final String TEST_PRODUCT_NAME = "test_product";
 
     @Autowired
     private UserService testedService;
@@ -70,5 +75,28 @@ public class UserServiceTest {
         assertThat(result.getUser(), is(admin));
         assertThat(result.isAuthorized(), is(true));
         assertThat(SecurityUtils.isAccessGranted(TOKEN), is(true));
+    }
+
+    @Test
+    public void shouldRefreshUserData(){
+        testedService.authenticate(ADMIN_USERNAME, CORRECT_PASSWORD, TOKEN);
+        mockUI(TOKEN);
+        Product product = new Product();
+        product.setName(TEST_PRODUCT_NAME);
+        admin.setProducts(Lists.newArrayList(product));
+
+        testedService.refreshUserData();
+
+        final UserDts result = SecurityUtils.getCurrentUser();
+        assertThat(result, is(notNullValue()));
+        assertThat(result.getUser().getProducts().stream().map(Product::getName).findFirst().get(), is(TEST_PRODUCT_NAME));
+    }
+
+    private void mockUI(String csrfToken) {
+        UI ui = mock(UI.class);
+        VaadinSession session = mock(VaadinSession.class);
+        when(ui.getSession()).thenReturn(session);
+        when(session.getCsrfToken()).thenReturn(csrfToken);
+        UI.setCurrent(ui);
     }
 }
