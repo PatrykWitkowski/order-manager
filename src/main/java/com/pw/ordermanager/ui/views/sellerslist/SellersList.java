302d804e343645f4e1930191979ff668829e3250
Patryk Witkowski <218167@student.pwr.edu.pl>
2018-09-26 19:59:26 +0200

    added sellers page to menu

---
+++ src/main/java/com/pw/ordermanager/ui/views/sellerslist/SellersList.java
@@ -0,0 +1,11 @@
+package com.pw.ordermanager.ui.views.sellerslist;
+
+import com.pw.ordermanager.ui.MainLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "sellers", layout = MainLayout.class)
+@PageTitle("Sellers List")
+public class SellersList extends VerticalLayout {
+}
