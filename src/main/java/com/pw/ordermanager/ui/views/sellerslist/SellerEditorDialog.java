5ff5a0c5ff25a4512b55ddde2214bea2d4a9093e
Patryk Witkowski <218167@student.pwr.edu.pl>
2018-09-26 23:05:37 +0200

    added NIP validator

--- src/main/java/com/pw/ordermanager/ui/views/sellerslist/SellerEditorDialog.java
+++ src/main/java/com/pw/ordermanager/ui/views/sellerslist/SellerEditorDialog.java
@@ -2,11 +2,22 @@ package com.pw.ordermanager.ui.views.sellerslist;
 
 import com.pw.ordermanager.backend.entity.Seller;
 import com.pw.ordermanager.ui.common.AbstractEditorDialog;
+import com.pw.ordermanager.ui.validators.NIPValidator;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class SellerEditorDialog extends AbstractEditorDialog<Seller> {
+
+    private TextField nipField = new TextField();
+    private TextField sellerName = new TextField();
+    private TextArea description = new TextArea();
+    private TextField addressStreet = new TextField();
+    private TextField addressNumber = new TextField();
+    private TextField addressPostalCode = new TextField();
+    private TextField addressLocation = new TextField();
 
     /**
      * Constructs a new instance.
@@ -15,11 +26,25 @@ public class SellerEditorDialog extends AbstractEditorDialog<Seller> {
      * @param itemDeleter
      */
     protected SellerEditorDialog(BiConsumer<Seller, Operation> itemSaver, Consumer<Seller> itemDeleter) {
-        super("", itemSaver, itemDeleter);
+        super("seller", itemSaver, itemDeleter);
+
+        createNipField();
+    }
+
+    private void createNipField() {
+        nipField.setLabel("NIP");
+        nipField.setRequired(true);
+        getFormLayout().add(nipField);
+
+        getBinder().forField(nipField)
+                .withValidator(new NIPValidator("Invalid NIP format or control sum."))
+                .bind(Seller::getNip, Seller::setNip);
     }
 
     @Override
     protected void confirmDelete() {
+        openConfirmationDialog("Delete seller",
+                "Are you sure you want to delete the seller: “" + getCurrentItem().getName() + "”?", "");
 
     }
 }
